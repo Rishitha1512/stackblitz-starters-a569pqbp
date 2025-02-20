8681ee16c0c8fe0e6f35f832219ac3fd465fda74
Rishitha1512 <katragadda.rishitha@kalvium.community>
2025-02-20 09:01:16 +0530

    completed practice assignment

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
+const jwt = require('jsonwebtoken');
+
+const SECRET_KEY = 'rishi@123'; 
+
 const encrypt = (payload) => {
-  // encrypt the payload and return token
-}
+  return jwt.sign(payload, SECRET_KEY, { expiresIn: '1hr' });
+};
 
 const decrypt = (token) => {
-  // return decoded payload
+  try {
+    return jwt.verify(token, SECRET_KEY);
+  } catch (err) {
+    return null;
+  }
+};
+
+const payload = { 
+  userId: 'Rishitha', 
+  role: 'Student' 
+};
+
+const token = encrypt(payload);
+console.log('Generated Token:', token);
+
+
+const decoded = decrypt(token);
+
+if (decoded) {
+  console.log('Decrypted Payload:', decoded);
+  console.log('Success');
+} else {
+  console.log('Failed to decrypt token');
 }
 
 module.exports = {
   encrypt,
   decrypt
-}
+};
